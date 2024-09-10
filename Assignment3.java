00017de6f17f501e013bd2a129e4b8a6856799a8
Arjun Gautam <166290097+Arjun039Gautam@users.noreply.github.com>
2024-09-10 15:42:52 +0530

    Final Pull request

--- Assignment3.java
+++ Assignment3.java
@@ -58,8 +58,9 @@ public class Assignment3 {
         }
     }
 
+    //Consecutive Number Summur
     public static void consecutiveNumber(int number){
-        for(int start = 1 ; start <= number ; start++){
+        for(int start = 1 ; start <= number / 2 ; start++){
             int sum = 0;
             for(int end = start ; end <= number ; end++){
                 sum += end ;
@@ -74,6 +75,7 @@ public class Assignment3 {
         }
     }
 
+    // method for print sequence
     public static void printSequence(int start, int end){
         for(int i = start ; i <= end ; i++){
             if(i == end){
@@ -84,61 +86,171 @@ public class Assignment3 {
         }
     }
 
+    // Encoding Challenge with ASCII Conversion
+    public static void encodingChallengeAsciiConversion(int[] array, int[] series) {
+        int n = array.length;
+
+        // Bubble sort in descending order
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    // Swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        String result = "";
+        for (int i = 0; i < series.length; i++) {
+            result += array[series[i] - 1] + 48;
+        }
+        System.out.println(result);
+    }
+
+    public static String encryptWithPattern(String input, int[] shiftPattern) {
+        String encrypted = "";
+        int patternLength = shiftPattern.length;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isLetter(c)) {
+                int shift = shiftPattern[i % patternLength];
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                c = (char) ((c - base + shift) % 26 + base);
+            }
+            encrypted += c;
+        }
+
+        return encrypted;
+    }
+
     public static void main(String[] args) {
         Constants constant = new Constants();
         Scanner input = new Scanner(System.in);
-        System.out.println(constant.OPERATION_TYPE);
-        System.out.print(constant.OPERATION_NO);
-        String operation = input.nextLine();
+        boolean isQuit = true;
+        while(isQuit){
+            System.out.println(constant.OPERATION_TYPE);
+            System.out.print(constant.OPERATION_NO);
+            String operation = input.nextLine();
 
-        switch (operation) {
-            case "1":
-                while (true) {
-                    try {
-                        System.out.print(constant.ENTER_POSITIVE);
-                        int number = input.nextInt();
-                        input.nextLine(); 
-                        if (number <= 0) {
-                            System.out.println(constant.MUST_POSITIVE);
-                            continue; 
+            switch (operation) {
+                case "1":
+                    while (true) {
+                        try {
+                            System.out.print(constant.ENTER_POSITIVE);
+                            int number = input.nextInt();
+                            input.nextLine(); 
+                            if (number <= 0) {
+                                System.out.println(constant.MUST_POSITIVE);
+                                continue; 
+                            }
+                            System.out.print(constant.OUTPUT);
+                            digitSumLoop(number);
+                            break; 
+                        } catch (InputMismatchException e) {
+                            System.out.println(constant.INVALID_INPUT_INTEGER);
+                            input.next(); 
                         }
-                        System.out.print(constant.OUTPUT);
-                        digitSumLoop(number);
-                        break; 
-                    } catch (InputMismatchException e) {
-                        System.out.println(constant.INVALID_INPUT_INTEGER);
-                        input.next(); 
                     }
-                }
-                break;
-        
-            case "2":
-                System.out.print(constant.ENTER_STRING);
-                String str = input.nextLine();
-                char[] chars = str.toCharArray();
-                System.out.println(constant.OUTPUT);
-                combine(chars, 0, "");
-                break;
-
-            case "3" :
-            while (true) {
-                try {
-                    System.out.print(constant.ENTER_POSITIVE);
-                    int number = input.nextInt();
-                    input.nextLine(); 
-                    if (number <= 0) {
-                        System.out.println(constant.MUST_POSITIVE);
-                        continue; 
-                    }
+                    break;
+            
+                case "2":
+                    System.out.print(constant.ENTER_STRING);
+                    String str = input.nextLine();
+                    char[] character = str.toCharArray();
                     System.out.println(constant.OUTPUT);
-                    consecutiveNumber(number);
-                    break; 
-                } catch (InputMismatchException e) {
-                    System.out.println(constant.INVALID_INPUT_INTEGER);
-                    input.next(); 
-                }
+                    combine(character, 0, "");
+                    break;
+
+                case "3" :
+                    while (true) {
+                        try {
+                            System.out.print(constant.ENTER_POSITIVE);
+                            int number = input.nextInt();
+                            input.nextLine(); 
+                            if (number <= 0) {
+                                System.out.println(constant.MUST_POSITIVE);
+                                continue; 
+                            }
+                            System.out.println(constant.OUTPUT);
+                            consecutiveNumber(number);
+                            break; 
+                        } catch (InputMismatchException e) {
+                            System.out.println(constant.INVALID_INPUT_INTEGER);
+                            input.next(); 
+                        }
+                    }
+                    break;
+                case "4" : 
+                    while (true) {
+                        try {
+                            System.out.print(constant.ENTER_ARRAY_SIZE);
+                            int arraySize = input.nextInt();
+                            input.nextLine();
+
+                            if (arraySize <= 0) {
+                                System.out.println(constant.MUST_POSITIVE);
+                                continue; 
+                            }
+
+                            int[] array = new int[arraySize];
+
+                            System.out.println(constant.ENTER_ARRAY_ELEMENT);
+                            for (int i = 0; i < arraySize; i++) {
+                                array[i] = input.nextInt();
+                            } 
+                            input.nextLine();
+                            
+                            System.out.print(constant.ENTER_SERIES_SIZE);
+                            int seriesSize = input.nextInt();
+                            input.nextLine();
+
+                            if (seriesSize <= 0) {
+                                System.out.println(constant.MUST_POSITIVE);
+                                continue; 
+                            }
+
+                            int[] series = new int[seriesSize];
+
+                            System.out.println(constant.ENTER_SERIES_ELEMENT);
+                            for (int i = 0; i < seriesSize; i++) {
+                                series[i] = input.nextInt();
+                            }
+                            input.nextLine();
+                            System.out.print(constant.OUTPUT);
+                            encodingChallengeAsciiConversion(array, series);;
+                            break; 
+                        } catch (InputMismatchException e) {
+                            System.out.println(constant.INVALID_INPUT_INTEGER);
+                            input.next(); 
+                        }
+                    }
+                    break ;
+                case "5" : 
+                    System.out.print(constant.ENCRIPT_STRING);
+                    String originalString = input.nextLine();
+                    input.nextLine();
+            
+                    // Input the shift pattern
+                    System.out.print(constant.SIZE_SHIFT);
+                    int patternSize = input.nextInt();
+                    input.nextLine();
+                    int[] shiftPattern = new int[patternSize];
+            
+                    System.out.println(constant.ENTER_SHIFT);
+                    for (int i = 0; i < patternSize; i++) {
+                        shiftPattern[i] = input.nextInt();
+                    }
+                    input.nextLine();
+
+                    String encrypted = encryptWithPattern(originalString, shiftPattern);
+                    System.out.println(constant.ENCRIPTED_STRING + encrypted);
+                    break ;
+                case "0" :
+                    isQuit = false;
+                    break ;
             }
-            break;
         }
         input.close();
     }
--- Constants.java
+++ Constants.java
@@ -8,5 +8,13 @@ public class Constants {
     String OUTPUT = "Output : ";
     String INVALID_INPUT_INTEGER = "Invalid input. Please enter an integer.";
     String ENTER_STRING = "Enter a string : ";
-    String OPERATION_TYPE = "Which operation you want to execute \n 1. Digit Sum Loop \n 2. Valid Parentheses Combination Generator";
+    String OPERATION_TYPE = "Which operation you want to execute \n 1. Digit Sum Loop \n 2. Valid Parentheses Combination Generator \n 3. Consecutive Number Summur \n 4. Encoding Challenge with ASCII Conversion \n 5. Caesar Cipher with Shift Variability \n 0. Exit";
+    String ENTER_ARRAY_SIZE = "Enter the size of the array: ";
+    String ENTER_ARRAY_ELEMENT = "Enter the elements of the array:";
+    String ENTER_SERIES_SIZE = "Enter the size of the series: ";
+    String ENTER_SERIES_ELEMENT = "Enter the elements of the array:";
+    String ENCRIPT_STRING = "Enter the string to encrypt: ";
+    String SIZE_SHIFT = "Enter the size of the shift pattern: ";
+    String ENTER_SHIFT = "Enter the shift pattern:";
+    String ENCRIPTED_STRING = "Encrypted string: ";
 }
